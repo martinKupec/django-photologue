ca706131e0344825836d265257bb12a4c3b8d2d4
Martin Kupec <martin.kupec@kupson.cz>
2012-04-16 00:47:23 +0200

    Updated gallery templates

--- photologue/templatetags/photologue_tags.py
+++ photologue/templatetags/photologue_tags.py
@@ -4,6 +4,19 @@ from photologue.models import Photo
 
 register = template.Library()
 
+@register.inclusion_tag('photologue/tags/gallery_list.html')
+def list_galleries(galleries, sample_size=None):
+    """ Return a list of galleries """
+    return {'object_list': galleries, 'sample_size': sample_size}
+
+@register.inclusion_tag('photologue/tags/gallery_item.html')
+def gallery_items(items, restrict=None):
+    """ Return a specified items """
+    if restrict:
+        return {'object_list': items[:restrict]}
+    else:
+        return {'object_list': items}
+
 @register.inclusion_tag('photologue/tags/next_in_gallery.html')
 def next_in_gallery(photo, gallery):
     return {'photo': photo.get_next_in_gallery(gallery)}
